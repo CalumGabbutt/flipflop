ae125e919fb2e42b36976b92f4e09d103a5a1af2
CalumGabbutt <c.gabbutt@qmul.ac.uk>
2021-04-20 17:12:41 +0100

    Restructured scripts

--- scripts/add_noise.py
+++ scripts/add_noise.py
@@ -1,49 +1,73 @@
+#!/usr/bin/env python3
+
 import pandas as pd
 import numpy as np
 from ticktockclock import ticktock
 import argparse
 
-parser = argparse.ArgumentParser(description='Apply noise model to a csv of "true" beta values')
-parser.add_argument('inputfile', type=str,
-                    help='path to csv file containing "true" beta values')
-parser.add_argument('outputfile', type=str, 
-                    help='path to csv file in which to store output')
-parser.add_argument('-delta', default=0.04, type=float, 
-                    help='offset from zero (default 0.04)') 
-parser.add_argument('-eta', default=0.92, type=float, 
-                    help='offset from one (default 0.92)')   
-parser.add_argument('-kappa', default=100, type=float, 
-                    help='sample size of beta distribution (default 100)\n(see https://en.wikipedia.org/wiki/Beta_distribution#Mean_and_sample_size)')   
-parser.add_argument('--index', action='store_true', default=False, dest='index',
-                    help='indicate whether the first column of the inputfile is the index of the csv (default False)')
-# Execute the parse_args() method
-args = parser.parse_args()
+def noise(beta_true, delta, eta, kappa):
 
-inputfile = args.inputfile
-outputfile = args.outputfile
-delta = float(args.delta)
-eta = float(args.eta)
-kappa = float(args.kappa)
-index = args.index
+    # Linear transform on beta to account for array saturating (e.g. shifts mean of
+    # lower peak from 0 to delta and upper peak from 1 to eta)
+    beta_rescale = ticktock.rescale_beta(beta_true, delta, eta)
 
-if index:
+    # Apply noise model (for each true beta value, draw the measured beta
+    # value from a beta distribution with a mean equal to the true beta value
+    # and a sample size equal to kappa)
+    beta_sample = ticktock.beta_rvs(beta_rescale, kappa)
+
+    return beta_sample
+
+def main():
+
+    # Initialise argparser
+    parser = argparse.ArgumentParser(description='Apply noise model to a csv of "true" beta values')
+    parser.add_argument('inputfile', type=str,
+                        help='path to csv file containing "true" beta values')
+    parser.add_argument('outputfile', type=str, 
+                        help='path to csv file in which to store output')
+    parser.add_argument('-delta', default=0.04, type=float, 
+                        help='offset from zero (default 0.04)') 
+    parser.add_argument('-eta', default=0.92, type=float, 
+                        help='offset from one (default 0.92)')   
+    parser.add_argument('-kappa', default=100, type=float, 
+                        help='sample size of beta distribution (default 100)\n(see https://en.wikipedia.org/wiki/Beta_distribution#Mean_and_sample_size)')   
+    parser.add_argument('--index', action='store_true', default=False, dest='index',
+                        help='indicate whether the first column of the inputfile is the index of the csv (default False)')
+
+    # Execute the parse_args() method
+    args = parser.parse_args()
+
+    inputfile = args.inputfile
+    outputfile = args.outputfile
+    delta = float(args.delta)
+    eta = float(args.eta)
+    kappa = float(args.kappa)
+    index = args.index
+
+    if (delta >= 1) | (delta <= 0):
+        raise Exception('delta must be between 0 and 1')
+    if (eta >= 1) | (eta <= 0):
+        raise Exception('eta must be between 0 and 1')
+    if (kappa <= 0):
+        raise Exception('kappa must be greater than 0')
+
     # Load csv file as dataframe
-    beta_true = pd.read_csv(inputfile, index_col=0)
-else:
-    beta_true = pd.read_csv(inputfile)
+    if index:
+        beta_true = pd.read_csv(inputfile, index_col=0)
+    else:
+        beta_true = pd.read_csv(inputfile)
 
-# Linear transform on beta to account for array saturating (e.g. shifts mean of
-# lower peak from 0 to delta and upper peak from 1 to eta)
-beta_rescale = ticktock.rescale_beta(beta_true, delta, eta)
+    # Add noise to the true beta values
+    beta_sample = noise(beta_true, delta, eta, kappa)
 
-# Apply noise model (for each true beta value, draw the measured beta
-# value from a beta distribution with a mean equal to the true beta value
-# and a sample size equal to kappa)
-beta_sample = ticktock.beta_rvs(beta_rescale, kappa)
+    # Save the noisy beta values
+    if index:
+        beta = pd.DataFrame(beta_sample, columns=beta_true.columns, index=beta_true.index)
+        beta.to_csv(outputfile)
+    else:
+        beta = pd.DataFrame(beta_sample, columns=beta_true.columns)
+        beta.to_csv(outputfile, index=False)
 
-if index:
-    beta = pd.DataFrame(beta_sample, columns=beta_true.columns, index=beta_true.index)
-    beta.to_csv(outputfile)
-else:
-    beta = pd.DataFrame(beta_sample, columns=beta_true.columns)
-    beta.to_csv(outputfile, index=False)
+if __name__ == "__main__":
+    main()
--- scripts/inference.py
+++ scripts/inference.py
@@ -8,55 +8,59 @@ import joblib
 
 import argparse
 
-parser = argparse.ArgumentParser(description='Run ticktock Bayesian inference.')
-parser.add_argument('datafile', type=str,
-                    help='path to csv containing beta values')
-parser.add_argument('patientinfofile', type=str,
-                    help='path to csv containing patientinfo')
-parser.add_argument('outputdir', type=str, default='~', 
-                    help='path to folder in which to store output')
-parser.add_argument('sample', type=str,
-                    help='samplename of beta array (must be a col in datafile index in patientinfo)')
-parser.add_argument('S', type=int,
-                    help='stem cell number to evaluate')
-parser.add_argument('-nlive', default=1500, dest='nlive', type=int,
-                    help='number of live points in dynesty sampler (default:1500)')
-parser.add_argument('--verbose', action='store_true', default=False, dest='verbose')
-parser.add_argument('-lamscale', default=1.0, type=float,
-                    help='scale of replacement rate (default:1.0)')
-parser.add_argument('-muscale', default=0.05, type=float,
-                    help='scale of methylation rate (default:0.05)')
-parser.add_argument('-gammascale', default=0.05, type=float,
-                    help='scale of methylation rate (default:0.05)')
+def main():
+    parser = argparse.ArgumentParser(description='Run ticktock Bayesian inference.')
+    parser.add_argument('datafile', type=str,
+                        help='path to csv containing beta values')
+    parser.add_argument('patientinfofile', type=str,
+                        help='path to csv containing patientinfo')
+    parser.add_argument('outputdir', type=str, default='~', 
+                        help='path to folder in which to store output')
+    parser.add_argument('sample', type=str,
+                        help='samplename of beta array (must be a col in datafile index in patientinfo)')
+    parser.add_argument('S', type=int,
+                        help='stem cell number to evaluate')
+    parser.add_argument('-nlive', default=1500, dest='nlive', type=int,
+                        help='number of live points in dynesty sampler (default:1500)')
+    parser.add_argument('--verbose', action='store_true', default=False, dest='verbose')
+    parser.add_argument('-lamscale', default=1.0, type=float,
+                        help='scale of replacement rate (default:1.0)')
+    parser.add_argument('-muscale', default=0.05, type=float,
+                        help='scale of methylation rate (default:0.05)')
+    parser.add_argument('-gammascale', default=0.05, type=float,
+                        help='scale of methylation rate (default:0.05)')
 
-# Execute the parse_args() method
-args = parser.parse_args()
+    # Execute the parse_args() method
+    args = parser.parse_args()
 
-datafile = args.datafile
-patientinfofile = args.patientinfofile
-outputdir = args.outputdir
-sample = args.sample
-S = int(args.S)
-nlive = int(args.nlive)
-verbose = args.verbose
-lamscale=float(args.lamscale)
-muscale=float(args.muscale)
-gammascale=float(args.gammascale)
+    datafile = args.datafile
+    patientinfofile = args.patientinfofile
+    outputdir = args.outputdir
+    sample = args.sample
+    S = int(args.S)
+    nlive = int(args.nlive)
+    verbose = args.verbose
+    lamscale=float(args.lamscale)
+    muscale=float(args.muscale)
+    gammascale=float(args.gammascale)
 
-outsamplesdir = os.path.join(outputdir, sample, 'posterior')
-outsamples = os.path.join(outsamplesdir, 'sample_{}.pkl'.format(S))
+    outsamplesdir = os.path.join(outputdir, sample, 'posterior')
+    outsamples = os.path.join(outsamplesdir, 'sample_{}.pkl'.format(S))
 
-os.makedirs(outsamplesdir, exist_ok=True)
+    os.makedirs(outsamplesdir, exist_ok=True)
 
-beta_values = pd.read_csv(datafile, index_col = 0)
-patientinfo = pd.read_csv(patientinfofile, keep_default_na=False, index_col = 0) 
+    beta_values = pd.read_csv(datafile, index_col = 0)
+    patientinfo = pd.read_csv(patientinfofile, keep_default_na=False, index_col = 0) 
 
-beta = beta_values[sample].dropna().values
-age = patientinfo.loc[sample, 'age']
+    beta = beta_values[sample].dropna().values
+    age = patientinfo.loc[sample, 'age']
 
-res = ticktock.run_inference(beta, age, S, nlive=nlive, 
-                            verbose=verbose, lamscale=lamscale, 
-                            muscale=muscale, gammascale=gammascale)
+    res = ticktock.run_inference(beta, age, S, nlive=nlive, 
+                                verbose=verbose, lamscale=lamscale, 
+                                muscale=muscale, gammascale=gammascale)
 
-with open(outsamples, 'wb') as f:
-    joblib.dump(res, f)
+    with open(outsamples, 'wb') as f:
+        joblib.dump(res, f)
+
+if __name__ == "__main__":
+    main()
